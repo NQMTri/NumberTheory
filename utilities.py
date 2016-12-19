c524176062a3e880d918e587d720c5a01d772fee
Bao Nguyen <nlbao95@gmail.com>
2016-12-20 00:50:48 +0700

    + Implement mpow(a, x, n) to calculate (a^x) mod n fastly. + Disable isPrime(n) because it will burn my computer.

--- utilities.py
+++ utilities.py
@@ -8,13 +8,25 @@ def mmod(a, n):
 	return ans
 
 
-def isPrime(n):
-	if n < 2:
-		return False
-	for i in range(2, n):
-		t = i*i
-		if t > n:
-			break
-		if (n%i) == 0:
-			return False
-	return True
+# def isPrime(n):
+# 	if n < 2:
+# 		return False
+# 	for i in range(2, n):
+# 		t = i*i
+# 		if t > n:
+# 			break
+# 		if (n%i) == 0:
+# 			return False
+# 	return True
+
+
+# calculate a^x (mod n)
+def mpow(a, x, n):
+	if x < 1:
+		return 1
+	a = mmod(a, n)
+	t = mpow(a, x>>1, n)
+	t = (t*t)%n
+	if x&1:
+		t = (t*a)%n
+	return t
