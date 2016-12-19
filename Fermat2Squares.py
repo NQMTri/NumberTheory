df8ed7be1fe009467e60dc27afacc6456c358f17
Bao Nguyen <nlbao95@gmail.com>
2016-12-20 00:51:28 +0700

    + Using mpow() to calculate x^((p-1)/4) mod p. + Skip prime checking.

--- Fermat2Squares.py
+++ Fermat2Squares.py
@@ -5,19 +5,21 @@ from utilities import *
 from EffectiveThueLemma import *
 
 
+# p is prime
 def findBeta(p):
 	beta = 0
-	while mmod(beta**2, p) != p-1:
+	p4 = (p-1)/4
+	while mpow(beta, 2, p) != p-1:
 		x = randrange(0, p)
-		beta = mmod(x**((p-1)/4), p)
+		beta = mpow(x, p4, p)
 	return beta
 
 
 # input: p is a prime that has form 4k+1.
 # output: two integers r and t such that r^2 + t^2 == p.
 def Fermat2Squares(p):
-	if not isPrime(p):
-		raise ValueError("%ld is not prime." % (p))
+	# if not isPrime(p):
+	# 	raise ValueError("%ld is not prime." % (p))
 	if mmod(p, 4) != 1:
 		raise ValueError("%ld does not have form 4k+1.")
 	beta = findBeta(p)
